a385a76efd0958b302d2d748d9d74b23458c81a9
martu <camposm@aia.es>
2021-04-22 18:05:10 +0200

    Modified setup files for PyPI package

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
+from distutils.core import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="cubix",
     version="1.0.4",
     author="Martin Campos",
